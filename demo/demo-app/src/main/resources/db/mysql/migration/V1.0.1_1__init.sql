8cf005b1b1e6170a1ec51113848c446f5f2c3f9a
xuwenjin <xuwjf@yonyou.com>
2018-02-27 16:23:54 +0800

    删除demo

--- demo/demo-app/src/main/resources/db/mysql/migration/V1.0.1_1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE PERSON (
-	first_name varchar(255) not null,
-	last_name varchar(255) not null
-);
-
-insert into PERSON (first_name, last_name) values ('Dave', 'Syer');
--- demo/demo-app/src/main/resources/db/mysql/migration/V1_0_1_2__init.sql
+++
@@ -1,2 +0,0 @@
-insert into PERSON (first_name, last_name) values ('Dave1', 'Syer1');
-	insert into PERSON (first_name, last_name) values ('Dave2', 'Syer2');
